cd974f4153cfe7d7e8d38da503e2f5ad17ddee30
AndreaFavullo <andreafavullo02@gmail.com>
2021-02-13 13:18:04 +0100

    Added comments
    Added comments about crate sound code

--- src/main/java/com/chuckita/steveandtheinfernaladventure/util/SoundRegisterListener.java
+++ src/main/java/com/chuckita/steveandtheinfernaladventure/util/SoundRegisterListener.java
@@ -6,12 +6,13 @@ import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class SoundRegisterListener {
-    @SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = true)
-    public void registerSoundEvents(RegistryEvent.Register<SoundEvent> event) {
-       event.getRegistry().registerAll(
-    		   SoundRegistrator.CRATE_PLACING,
-    		   SoundRegistrator.CRATE_BREAK,
-    		   SoundRegistrator.CRATE_BREAKING
-       );
-    }   
+	@SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = true)
+	public void registerSoundEvents(RegistryEvent.Register<SoundEvent> event) {
+		//registro tutti i suoni
+		event.getRegistry().registerAll(
+				SoundRegistrator.CRATE_PLACING,
+				SoundRegistrator.CRATE_BREAK,
+				SoundRegistrator.CRATE_BREAKING
+		);
+	}
 }
--- src/main/java/com/chuckita/steveandtheinfernaladventure/util/SoundRegistrator.java
+++ src/main/java/com/chuckita/steveandtheinfernaladventure/util/SoundRegistrator.java
@@ -7,20 +7,26 @@ import net.minecraft.util.SoundEvent;
 
 public class SoundRegistrator {
 	
+	//dichiaro i suoni
 	public static final SoundEvent CRATE_PLACING;
 	public static final SoundEvent CRATE_BREAK;
 	public static final SoundEvent CRATE_BREAKING;
 	
 	static {
+		//li aggiungo al registry
 		CRATE_PLACING = addSoundsToRegistry("crate_placing");
 		CRATE_BREAK = addSoundsToRegistry("crate_break");
 		CRATE_BREAKING = addSoundsToRegistry("crate_breaking");
     }
 	
 	private static SoundEvent addSoundsToRegistry(String soundID) {
+		//recupero il suono
 		ResourceLocation shotSoundLocation = new ResourceLocation(SteveAndTheInfernalAdventure.MOD_ID, soundID);
+		//creo il SoundEvent con quel suono
 		SoundEvent soundEvent = new SoundEvent(shotSoundLocation);
+		//lo aggiungo al registry
 		soundEvent.setRegistryName(shotSoundLocation);
+		//restituisco Sound Event
 		return soundEvent;
 	}
 }
